d82a07ff471ab2f16414fd27045d9076799e1733
Jani H <herranen.jani@gmail.com>
2019-12-09 21:09:09 +0200

    Simple angular todo frontend application

---
+++ src/app/todo.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class TodoService {
+  private baseUrl = 'http://localhost:8080';
+
+  constructor(private http: Http) { }
+
+  getTodos(): Promise<Todo[]> {
+    return this.http.get(this.baseUrl + '/api/todos/')
+      .toPromise()
+      .then(response => response.json() as Todo[])
+      .catch(this.handleError);
+  }
+
+  createTodo(todoData: Todo): Promise<Todo> {
+    return this.http.post(this.baseUrl + '/api/todos/', todoData)
+      .toPromise().then(response => response.json() as Todo)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Some error occured', error);
+    return Promise.reject(error.message || error);
+  }
+}
